b18a2c4f26eda694c29ffbbd084eb4867ee15af2
farhan <farhan.aqeel@hotmail.com>
2018-01-15 17:07:37 +0500

    constant done.

---
+++ 06 - ConstantsAndAdditionalOperators.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+let canChange = false
+
+if !canChange{
+    print("Cannot change it mate! ")
+}
+
+let securityCheck = true
+let scanComplete = false
+let vipGuy = true
+
+
+if (securityCheck && scanComplete) || vipGuy {
+print("You can go duckhead!")
+}
+
---
+++ ConditionalLogicsAndStatements.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var areWeCool = false
+
+
+if areWeCool == true || areWeCool != false{
+    print("what just happened???")
+}else{
+    print("If statement was not true to it printed this!")
+}
+
+
+
+if 3 < 4{
+    print("3 is surely less so we should see this")
+}
+if 3 > 4 {
+    print("shouldn't see this as 3 is not greater than 4")
+}
+
+if 3 == 3{
+    print("should see this as 3 is equal to 3")
+}
+if 3 != 4{
+    print("lets see this as 3 is not equals to 4.")
+}
+
+var isThreeGreaterThanThree = 3 > 3
+var isTheNameCorrent = "geeks" == "Geeks"
+
